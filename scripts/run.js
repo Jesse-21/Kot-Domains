d1ca1f314dd87a224fb0ace968339fbea6c450ed
tony <antolisah@gmail.com>
2022-03-03 06:10:38 +0300

    non-fungible domain

--- scripts/run.js
+++ scripts/run.js
@@ -1,16 +1,22 @@
 const main = async () => {
-  const [owner, randomPerson] = await hre.ethers.getSigners(); // gets wallet address of the owner
+  const [owner, randomPerson] = await hre.ethers.getSigners(); // gets wallet address of the owner, and a random person
   const domainContractFactory = await hre.ethers.getContractFactory("Domains");
-  const domainContract = await domainContractFactory.deploy();
+
+  // we pass in "kot" to the constructor, which is the name of the domain
+  const domainContract = await domainContractFactory.deploy("kot");
   await domainContract.deployed();
+
   console.log('Deployed domain contract to: ', domainContract.address);
   console.log('Contract deployed by: ', owner.address);
 
-  const txn = await domainContract.register('doom');
+  const txn = await domainContract.register('young', { value: hre.ethers.utils.parseEther('0.1') });
   await txn.wait();
 
-  const domainOwner = await domainContract.getAddress('doom');
-  console.log('Domain owner: ', domainOwner);
+  const address = await domainContract.getAddress('young');
+  console.log('owner of domain young: ', address);
+
+  const balance = await hre.ethers.provider.getBalance(domainContract.address);
+  console.log('Contract balance: ', hre.ethers.utils.formatEther(balance));
 };
 
 const runMain = async () => {
