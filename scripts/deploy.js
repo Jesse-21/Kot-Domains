920f517f642c20e103fb49ac0babbe7dc2490caf
Jesse21 <98306896+Jesse-21@users.noreply.github.com>
2022-11-04 16:59:59 -0400

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,22 +1,22 @@
 const main = async () => {
   const domainContractFactory = await hre.ethers.getContractFactory("Domains");
-  // we pass in "kot" to the constructor, which is the name of the domain
-  const domainContract = await domainContractFactory.deploy("kot");
+  // we pass in "artist" to the constructor, which is the name of the domain
+  const domainContract = await domainContractFactory.deploy("artist");
   await domainContract.deployed();
 
   console.log('Deployed domain contract to: ', domainContract.address);
 
-  let txn = await domainContract.register('hawkeye', { value: hre.ethers.utils.parseEther('0.1') });
+  let txn = await domainContract.register('jesse21', { value: hre.ethers.utils.parseEther('0.1') });
   await txn.wait();
-  console.log("minted domain hawkeye.kot");
+  console.log("minted domain jesse21.artist");
 
   // set record
-  txn = await domainContract.setRecord('hawkeye', 'I am a great archer!');
+  txn = await domainContract.setRecord('Jesse21', 'J21!');
   await txn.wait();
-  console.log("set record for domain hawkeye.kot");
+  console.log("set record for domain jesse21.artist");
 
-  const address = await domainContract.getAddress('hawkeye');
-  console.log('owner of domain hawkeye: ', address);
+  const address = await domainContract.getAddress('jesse21');
+  console.log('owner of domain jesse21: ', address);
 
   const balance = await hre.ethers.provider.getBalance(domainContract.address);
   console.log('Contract balance: ', hre.ethers.utils.formatEther(balance));
